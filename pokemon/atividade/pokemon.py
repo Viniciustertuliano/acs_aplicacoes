bf1ed2bd58c21205edc58b717c06e5828f2b65d7
vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2020-09-15 12:16:46 -0300

    finalizado exercicios de 1 ao 4

--- pokemon/atividade/pokemon.py
+++ pokemon/atividade/pokemon.py
@@ -36,35 +36,49 @@ site_pokeapi = "https://pokeapi.co"
 """
 Vamos precisar destas quatro exceções personalizadas.
 """
+
+
 class PokemonNaoExisteException(Exception):
-    raise
+    pass
+
 
 class PokemonNaoCadastradoException(Exception):
     pass
 
+
 class TreinadorNaoCadastradoException(Exception):
     pass
+
 
 class PokemonJaCadastradoException(Exception):
     pass
 
+
 """
 Esta função certifica-se de que seu parâmetro é um número inteiro e lança uma ValueError se não for.
 """
+
+
 def check_int(a):
     if type(a) is not int:
         raise ValueError()
 
+
 """
 Esta função certifica-se de que seu parâmetro é uma string e que não está vazia e lança uma ValueError se não for.
 """
+
+
 def check_str(a):
     if type(a) is not str or a == "":
         raise ValueError()
 
+
 """
 Esta classe será utilizada no exercício 12 abaixo.
 """
+
+
 @dataclass()
 class Pokemon:
     nome_treinador: str
@@ -75,34 +89,70 @@ class Pokemon:
     cor: str
     evoluiu_de: str
 
+
 """
 1. Dado o número de um pokémon, qual é o nome dele?
 """
+
+
 def nome_do_pokemon(numero):
     dic = requests.get(f'http://pokeapi.co/api/v2/pokemon/{numero}/').json()
     return dic['name']
 
+
 """
 2. Dado o nome de um pokémon, qual é o número dele?
 """
+
+
 def numero_do_pokemon(nome):
-    dic = requests.get(f'http://pokeapi.co/api/v2/pokemon/{str(nome).lower()}/').json()
+    r = requests.get(
+        f"https://pokeapi.co/api/v2/pokemon-species/{str(nome).lower()}/")
+
+    if r.status_code != 200:
+        raise PokemonNaoExisteException
+    dic = r.json()
     return dic['id']
+
 
 """
 3. Dado o nome ou número de um pokémon, qual é o nome da cor (em inglês) predominante dele?
 """
+
+
 def color_of_pokemon(nome):
-    dic = requests.get(f'http://pokeapi.co/api/v2/pokemon-species/{nome}/').json()
-    return(dic['color']['name'])
+    r = requests.get(
+        f"https://pokeapi.co/api/v2/pokemon-species/{str(nome).lower()}/")
+
+    if r.status_code != 200:
+        raise PokemonNaoExisteException
+
+    dic = r.json()
+    return dic['color']['name']
+
 
 """
 4. Dado o nome ou número de um pokémon, qual é o nome da cor (em português) predominante dele?
 Os nomes de cores possíveis em português são "marrom", "amarelo", "azul", "rosa", "cinza", "roxo", "vermelho", "branco", "verde" e "preto".
 No entanto, a pokeapi ainda não foi traduzida para o português! Como você pode dar um jeito nisso?
 """
+
+
 def cor_do_pokemon(nome):
-    pass
+    dic_color = {"brown": "marrom",
+                 "green": "verde",
+                 "red": "vermelho",
+                 "blue": "azul",
+                 "yellow": "amarelo",
+                 "pink": "rosa",
+                 "black": "preto",
+                 "gray": "cinza",
+                 "white": "branco",
+                 "purple": "roxo"}
+    color = color_of_pokemon(nome)
+    print(color)
+    return dic_color[color]
+
 
 """
 5. Dado o nome de um pokémon, quais são os tipos no qual ele se enquadra?
@@ -110,16 +160,22 @@ Os nomes dos tipos de pokémons em português são "normal", "lutador", "voador"
 Todo pokémon pode pertencer a um ou a dois tipos diferentes. Retorne uma lista contendo os tipos, mesmo que haja somente um.
 Se houver dois tipos, a ordem não é importante.
 """
+
+
 def tipos_do_pokemon(nome):
     pass
+
 
 """
 6. Dado o nome de um pokémon, liste de qual pokémon ele evoluiu.
 Por exemplo, evolucao_anterior('venusaur') == 'ivysaur'
 Retorne None se o pokémon não tem evolução anterior. Por exemplo, evolucao_anterior('bulbasaur') == None
 """
+
+
 def evolucao_anterior(nome):
     pass
+
 
 """
 7. Dado o nome de um pokémon, liste para quais pokémons ele pode evoluiur.
@@ -130,8 +186,11 @@ evolucoes_proximas('poliwhirl') == ['poliwrath', 'politoed']
 Note que esta função dá como resultado somente o próximo passo evoluitivo. Assim sendo, evolucoes_proximas('poliwag') == ['poliwhirl']
 Se o pokémon não evolui, retorne uma lista vazia. Por exemplo, evolucoes_proximas('celebi') == []
 """
+
+
 def evolucoes_proximas(nome):
     pass
+
 
 """
 8. A medida que ganham pontos de experiência, os pokémons sobem de nível.
@@ -140,15 +199,21 @@ Entretanto, cada tipo de pokémon adota uma curva de level-up diferente (na verd
 Assim sendo, dado um nome de pokémon e uma quantidade de pontos de experiência, retorne o nível em que este pokémon está.
 Valores negativos de experiência devem ser considerados inválidos.
 """
+
+
 def nivel_do_pokemon(nome, experiencia):
     pass
+
 
 """
 9. Dado um nome de treinador, cadastre-o na API de treinador.
 Retorne True se um treinador com esse nome foi criado e False em caso contrário (já existia).
 """
+
+
 def cadastrar_treinador(nome):
     pass
+
 
 """
 10. Imagine que você capturou dois pokémons do mesmo tipo. Para diferenciá-los, você dá nomes diferentes (apelidos) para eles.
@@ -156,42 +221,55 @@ Logo, um treinador pode ter mais do que um pokémon de um determinado tipo, mas 
 Assim sendo, dado um nome de treinador, um apelido de pokémon, um tipo de pokémon e uma quantidade de experiência, cadastre o pokémon com o tipo correspondente ao treinador dado na API do treinador.
 Certifique-se de que todos os dados são válidos.
 """
+
+
 def cadastrar_pokemon(nome_treinador, apelido_pokemon, tipo_pokemon, experiencia):
     pass
+
 
 """
 11. Dado um nome de treinador, um apelido de pokémon e uma quantidade de experiência, localize esse pokémon e acrescente-lhe a experiência ganha.
 """
+
+
 def ganhar_experiencia(nome_treinador, apelido_pokemon, experiencia):
     pass
+
 
 """
 12. Dado um nome de treinador e um apelido de pokémon, localize esse pokémon na API do treinador e retorne um objeto da classe Pokemon, prenchida com os atributos definidos na classe
 """
+
+
 def localizar_pokemon(nome_treinador, apelido_pokemon):
     pass
+
 
 """
 13. Dado o nome de um treinador, localize-o na API do treinador e retorne um dicionário contendo como chaves, os apelidos de seus pokémons e como valores os tipos deles.
 """
+
+
 def detalhar_treinador(nome_treinador):
     pass
+
 
 """
 14. Dado o nome de um treinador, localize-o na API do treinador e exclua-o, juntamente com todos os seus pokémons.
 """
+
+
 def excluir_treinador(nome_treinador):
     pass
+
 
 """
 15. Dado o nome de um treinador e o apelido de um de seus pokémons, localize o pokémon na API do treinador e exclua-o.
 """
+
+
 def excluir_pokemon(nome_treinador, apelido_pokemon):
     pass
 
 
-nome_do_pokemon(56)
-
-numero_do_pokemon('EEVEE')
-
-color_of_pokemon('eevee')
+cor_do_pokemon("magneton")
